5d6fdd4f8f5422ca710c14c7646e8071469b562c
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-05-31 16:45:51 +0530

    Update Conversion.java

--- Conversion.java
+++ Conversion.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.time.ZoneOffset;
+import java.util.Arrays;
 
 public class Conversion {
 
@@ -24,11 +26,21 @@ public class Conversion {
     }
 
     public static void main(String[] args) {
-        TimeZone t1 = new TimeZone(originalTimeZone);
-        TimeZone t2 = new TimeZone(changedTimeZone);
+        //TimeZone t1 = new TimeZone(originalTimeZone);
+        //TimeZone t2 = new TimeZone(changedTimeZone);
+        TimeZone t1 = new TimeZone("America/Montreal");
+        TimeZone t2 = new TimeZone("Europe/Paris");
         ZoneOffset z1 = t1.getZoneOffset();
         ZoneOffset z2 = t2.getZoneOffset();
-
-
+        String s1 = String.valueOf(z1);
+        String s2 = String.valueOf(z2);
+        String[] v1 = s1.split(":");
+        String[] v2 = s2.split(":");
+        System.out.println(Arrays.toString(v1));
+        System.out.println(Arrays.toString(v2));
+        int a = Integer.parseInt(v2[0]) - Integer.parseInt(v1[0]);
+        int b = Integer.parseInt(v2[1]) - Integer.parseInt(v1[1]);
+        System.out.println(a);
+        System.out.println(b);
     }
 }
