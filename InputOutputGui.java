595d66fdedb121646f3e8b725886e961174fb818
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-06-01 19:42:20 +0530

    Create InputOutputGui.java

---
+++ InputOutputGui.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class InputOutputGui {
+    public static void main(String[] args) {
+        TimeZone t = new TimeZone();
+        String[] list = t.getListOfZoneId();
+        String originalTimeZone;
+    }
+}
