e4545343f23e80c536ead24253f2390251feac76
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-06-02 16:58:57 +0530

    Update InputOutputGui.java

--- InputOutputGui.java
+++ InputOutputGui.java
@@ -3,8 +3,18 @@ import java.awt.*;
 
 public class InputOutputGui {
     public static void main(String[] args) {
-        TimeZone t = new TimeZone();
+        TimeZone t = new TimeZone("Europe/Paris");
         String[] list = t.getListOfZoneId();
         String originalTimeZone;
+        JFrame jFrame = new JFrame();
+        jFrame.setTitle("Time Change");
+        jFrame.setResizable(false);
+        jFrame.setSize(540, 480);
+        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        jFrame.setVisible(true);
+        JPanel jPanel = new JPanel();
+        JButton jButton = new JButton();
+        jPanel.add(jButton);
+        jFrame.add(jPanel);
     }
 }
