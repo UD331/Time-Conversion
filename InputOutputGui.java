9908651a521584fb196b13916313e4256f174e1f
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-06-03 21:20:31 +0530

    Update InputOutputGui.java

--- InputOutputGui.java
+++ InputOutputGui.java
@@ -6,11 +6,18 @@ public class InputOutputGui {
         TimeZone t = new TimeZone("Europe/Paris");
         String[] list = t.getListOfZoneId();
         String originalTimeZone;
+        String[] s = {"Change Current System Time", "Change a Specific Time"};
+        String a;
+        a = JOptionPane.showInputDialog(null, "Please select in" +
+                        " which manner you would like to make changes",
+                "Time Conversion", JOptionPane.QUESTION_MESSAGE, s, s[0]);
         JFrame jFrame = new JFrame();
         jFrame.setTitle("Time Change");
         jFrame.setResizable(false);
         jFrame.setSize(540, 480);
         jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        jFrame.setLayout(new GridLayout(4,0));
+        jFrame.pack();
         jFrame.setVisible(true);
         JPanel jPanel = new JPanel();
         JButton jButton = new JButton();
